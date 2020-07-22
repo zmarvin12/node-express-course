3b6cd3dc038844bd672dc6e4ac8fe784fb89e4d6
zmarvin12 <zackmarv12@gmail.com>
2020-07-22 10:12:41 -0400

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name: 'Mark'},
@@ -14,14 +16,37 @@ app.get('/users', function (req, res){
     })
 })
 
-app.listen(8000, function () {
-    console.log("server is running")
-    app.get('/users/:id',function(req,res){
-        console.log(req.params.id)
+app.get('/users/:id',function(req,res){
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
+})
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername ="billyTheKid";
+    const mockPassword ="superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
         res.json({
             success: true,
-            message: 'got one user',
-            user: req.params.id
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
         })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
     })
+    }
+})
+
+app.listen(8000, function () {
+    console.log("server is running")
+
   })
